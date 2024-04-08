ef2059e2ea78eeb912d5eaa983339dc8e1b4e16b
EmyAssis75 <166054739+EmyAssis75@users.noreply.github.com>
2024-04-08 14:00:07 -0300

    conexao.php

--- conexao.php
+++ conexao.php
@@ -1,1 +1,10 @@
-
+<?php
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=wiliamc', 'root', '');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo ("Conectado 2AB - O MELHOR");
+} catch (PDOException $e) {
+    echo 'Erro: ' . $e->getMessage();
+    echo ("Não conectado");
+}
+?>
